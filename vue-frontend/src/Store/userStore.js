1fe3deef04ea381dd6c79392aecd89be279b6877
JeWook Lee <123925291+nabidana@users.noreply.github.com>
2023-11-14 16:39:08 +0900

    update

--- vue-frontend/src/Store/userStore.js
+++ vue-frontend/src/Store/userStore.js
@@ -3,15 +3,41 @@ import axios from "axios";
 export const userStore = {
     namespaced : true,
     state : () => ({
-
+        test : 0,
+        test1 : {
+            test1_a : 'a',
+            test1_b : 'b',
+        }
     }),
     getters : {
-
+        getTest : state => state.test,
+        getTest1 : state => state.test1,
+        getTest1_a : state => state.test1['test1_a'],
     },
     mutations : {
-
+        setTest : (state, data) => {
+            state.test = data
+        },
+        setTest1 : (state, data) => {
+            state.test1['test1_a'] = data['a']
+            state.test1['test1_b'] = data['b']
+        }
     },
     actions : {
-        
+        getAPIfromServer : context => {
+            axios({
+                url:'',
+                method:'POST',
+                headers:{
+                    'Content-type': 'application/json',
+                },
+                data:JSON.stringify({
+                    'test':'test'
+                })
+            })
+            .then( (res) => {
+                console.log(res);
+            })
+        }
     }
 }
---
+++ vue-frontend/src/components/StoreTest.vue
@@ -0,0 +1,29 @@
+<template>
+    <!-- Store 관련 테스트를 위해서, 작성되었습니다. 
+        userStore 만 작성되어 있습니다.
+    -->
+</template>
+<script setup>
+import { useStore } from 'vuex';
+import { ref } from 'vue';
+const store = useStore();
+const test = store.getters['userStore/getTest']
+const testArr = ref({})
+testArr.value = store.getters['userStore/getTest1']
+
+const testSet = (num) => {
+    store.commit('userStore/setTest', num)
+}
+const testSet2 = () => {
+    let arr = {
+        'a' : 'q',
+        'b' : 'w',
+    }
+
+    store.commit('userStore/setTest1', arr)
+}
+
+const doAxios = () => {
+    store.dispatch('userStore/getAPIfromServer')
+}
+</script>
